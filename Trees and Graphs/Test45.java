e8d431c765fda51a21bd63c250c4ce02c7459dc2
Neeraj Lad <neeraj.lad@yandex.com>
2016-06-10 23:01:19 +0530

    Trees and Graphs

---
+++ Trees and Graphs/Test45.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int d)
+    {
+        data = d; 
+    }
+}
+
+class BST
+{
+    Node root;
+
+    Node insert(Node root, int data)
+    {
+        if (root == null)
+            return new Node(data);
+        else
+        {
+            if (root.data > data)
+                root.left = insert(root.left, data);
+            else
+                root.right = insert(root.right, data);
+        }
+        return root;
+    }
+
+    void inorderTraversal(Node root)
+    {
+        if (root != null)
+        {
+            inorderTraversal(root.left);
+            System.out.print(root.data + " "); 
+            inorderTraversal(root.right);
+        }
+    }
+
+    boolean isBST(Node root, int min, int max)
+    {
+        if (root == null)
+            return true;
+
+        if (root.data < min || root.data > max)
+            return false;
+
+        return (isBST(root.left, min, root.data-1) && isBST(root.right, root.data+1, max));
+    }
+}
+
+class Test45
+{
+    public static void main(String args[])throws IOException
+    {
+       BST bst = new BST();
+       bst.root = bst.insert(bst.root, 20);
+       bst.root = bst.insert(bst.root, 10);
+       bst.root = bst.insert(bst.root, 13);
+       bst.root = bst.insert(bst.root, 23);
+       bst.root = bst.insert(bst.root, 38);
+       bst.root.right.right.right = new Node(15);
+
+       bst.inorderTraversal(bst.root);
+       System.out.println();
+
+       if (bst.isBST(bst.root, Integer.MIN_VALUE, Integer.MAX_VALUE))
+            System.out.println("Given tree is a BST");
+       else
+            System.out.println("Given tree is not a BST");
+    }
+}
+
---
+++ Trees and Graphs/Test46.java
@@ -0,0 +1,123 @@
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left, right, parent;
+
+    Node(int d)
+    {
+        data = d; 
+    }
+}
+
+class BST
+{
+    Node root;
+
+    Node insert(Node root, int data)
+    {
+        if (root == null)
+            return new Node(data);
+        else
+        {
+            Node t;
+            if (root.data > data)
+            {
+                t = insert(root.left, data);
+                t.parent = root;
+                root.left = t;
+            }
+            else
+            if (root.data < data)
+            {
+                t = insert(root.right, data);
+                t.parent = root;
+                root.right = t;
+            }
+            return root;
+        }
+    }
+
+    Node search(Node root, int data)
+    {
+        if (root == null)
+            return null;
+
+        if (root.data == data)
+            return root;
+
+        if (root.data > data)
+            return search(root.left, data);
+        else
+        if (root.data < data)
+            return search(root.right, data);
+
+        return null;
+    }
+
+    void inorderTraversal(Node root)
+    {
+        if (root != null)
+        {
+            inorderTraversal(root.left); 
+            System.out.print(root.data + " ");
+            inorderTraversal(root.right); 
+        }
+    }
+
+    Node minNode(Node n)
+    {
+        Node t = n;
+        while (t.left != null)
+            t = t.left;
+        return t;
+    }
+
+    Node inorderSuccessor(Node n)
+    {
+        if (n == null)
+            return null;
+        if (n.right != null)
+            return minNode(n.right);
+        else
+        {
+            Node p = n.parent;
+            while (p != null && n == p.right)
+            {
+                n = p;
+                p = p.parent;
+            }
+            return p;
+        }
+    }
+}
+
+class Test46
+{
+    public static void main(String args[])throws IOException
+    {
+       BST bst = new BST();
+       bst.root = bst.insert(bst.root, 20);
+       bst.root = bst.insert(bst.root, 10);
+       bst.root = bst.insert(bst.root, 23);
+       bst.root = bst.insert(bst.root, 38);
+       bst.root = bst.insert(bst.root, 17);
+       bst.root = bst.insert(bst.root, 9);
+       bst.root = bst.insert(bst.root, 25);
+       bst.root = bst.insert(bst.root, 2);
+
+       bst.inorderTraversal(bst.root);
+       System.out.println();
+
+       Node t = bst.search(bst.root, 2);
+
+       Node s = bst.inorderSuccessor(t);  
+       if (s != null)
+           System.out.println("Inorder Successor of " + t.data + " is: " + s.data);
+       else
+           System.out.println("Inorder Successor of " + t.data + " does not exist");
+    }
+}
+
